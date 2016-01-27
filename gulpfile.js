61af3c2064a9272edbbe781f8a1a075686d2222a
Denis Kurilenko <kurilenkodenis@gmail.com>
2016-01-27 21:14:09 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var cssnano = require('gulp-cssnano');
+
+gulp.task('scripts', function() {
+  return gulp.src([
+      "./node_modules/jquery/dist/jquery.js",      
+      "./node_modules/bootstrap/dist/js/bootstrap.js",
+      "./node_modules/angular/angular.js",      
+      "./frontend/js/*.js"])
+      .pipe(plumber())
+      .pipe(uglify())
+      .pipe(concat("main.js"))
+      .pipe(gulp.dest("./public/js/"));
+});
+
+gulp.task('styles', function() {
+  return gulp.src([
+      "./node_modules/bootstrap/dist/css/bootstrap.css", 
+      "./frontend/css/*.css"])    
+      .pipe(plumber())
+      .pipe(cssnano())
+      .pipe(concat("main.css"))
+      .pipe(gulp.dest("./public/css/"));
+});
+
+gulp.task('default', ['styles','scripts']);
+
+gulp.task('watch', ['default'], function(){
+    gulp.watch("./frontend/js/*.js", ["scripts"]);
+    gulp.watch("./frontend/css/*.css", ["styles"]);
+});
