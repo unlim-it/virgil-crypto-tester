482d7ace6fd0199e4243928e0449429d8af641f8
Denis Kurilenko <kurilenkodenis@gmail.com>
2016-01-28 19:53:37 +0200

    added logs to the form and stopwatch timer

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -3,13 +3,28 @@
 
 var cryptoTester = angular.module('cryptoTester',[]);
 
-cryptoTester.controller('mainCtrl', ['$scope', '$sce', function($scope, $sce) {
+cryptoTester.controller('mainCtrl', ['$scope', function($scope) {
     
     $scope.text = '';
+    $scope.logs = [];  
     
-    $scope.log = function(text){
-        $scope.output += $sce.trustAsHtml('<p>' + text + '</p>'); 
+    var startTime;
+    
+    var startWatch = function(){
+        startTime = performance.now();
     };
+    
+    var stopWatch = function(){        
+        var t = startTime;
+        startTime = 0;
+        return Math.round(performance.now() - t);
+    };
+    
+    var outputLog = function(log){
+        $scope.$apply(function() {
+            $scope.logs.push(log);                        
+        });
+    };    
     
     $scope.startBenchmark = function(){
         
@@ -17,18 +32,44 @@ cryptoTester.controller('mainCtrl', ['$scope', '$sce', function($scope, $sce) {
         var virgil = new Virgil('NO_KEYS');
         
         var keyPair;
+        var cipherData;
+        var signData;
         
-        $scope.log("Generating key pair...")
+        // clear logs before start benchmark
+        $scope.logs.length = 0;                
+        $scope.logs.push({ desc: "Benchmark started..." }); 
         
-        virgil.crypto.generateKeyPairAsync().then(function(keys){
+        startWatch();
+                
+        // Key Pair generation
+        virgil.crypto.generateKeyPairAsync().then(function(keys){            
             keyPair = keys;
-            console.log(keyPair);
-            return virgil.crypto.encryptStringToBase64Async($scope.text, 'RecipientId', keyPair.publicKey);
-        }).then(function(encryptedText){
-            console.log(encryptedText);
-            return virgil.crypto.decryptStringFromBase64Async(encryptedText, 'RecipientId', keyPair.privateKey)
-        }).then(function(decryptedText){
-            console.log(decryptedText);
+            outputLog({ desc: "Public/Private key pair generation", result: stopWatch() + 'ms' });
+            startWatch();
+            return virgil.crypto.encryptAsync($scope.text, 'RecipientId', keyPair.publicKey);
+        // Encryption
+        }).then(function(encryptedData){
+            outputLog({ desc: "Encryption", result: stopWatch() + 'ms' });     
+            cipherData = encryptedData;
+            startWatch();
+            return virgil.crypto.signAsync(encryptedData, keyPair.privateKey);
+        // Signing           
+        }).then(function(signDigest){
+            outputLog({ desc: "Signing", result: stopWatch() + 'ms' });            
+            signData = signDigest;           
+            startWatch();                                
+            return virgil.crypto.verifyAsync(cipherData, keyPair.publicKey, signData);
+        // Virification 
+        }).then(function(isValid){          
+            outputLog({ desc: "Verification", result: stopWatch() + 'ms' });     
+            startWatch();         
+            return virgil.crypto.decryptAsync(cipherData, 'RecipientId', keyPair.privateKey);
+        // Decryption  
+        }).then(function(decryptedText){      
+            outputLog({ desc: "Decryption", result: stopWatch() + 'ms' });
+        }).catch(function(ex){
+            $scope.error = ex.message;
+            alert(ex.message);
         });
     };
 }]);
