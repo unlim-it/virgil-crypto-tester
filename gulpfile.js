fefd94107ff78a3032433ce0f5eefaea1c12e5f4
Denis Kurilenko <Denis Kurilenko>
2016-01-28 03:26:43 +0200

    added infrastructure and all required dependencied

--- gulpfile.js
+++ gulpfile.js
@@ -9,10 +9,20 @@ gulp.task('scripts', function() {
       "./node_modules/jquery/dist/jquery.js",      
       "./node_modules/bootstrap/dist/js/bootstrap.js",
       "./node_modules/angular/angular.js",      
+      "./node_modules/virgil-sdk/dist/virgil-sdk.dev.js",      
       "./frontend/js/*.js"])
       .pipe(plumber())
       .pipe(uglify())
       .pipe(concat("main.js"))
+      .pipe(gulp.dest("./public/js/"));
+});
+
+gulp.task('app', function() {
+  return gulp.src([     
+      "./frontend/js/*.js"])
+      .pipe(plumber())
+      //.pipe(uglify())
+      .pipe(concat("app.js"))
       .pipe(gulp.dest("./public/js/"));
 });
 
@@ -26,9 +36,9 @@ gulp.task('styles', function() {
       .pipe(gulp.dest("./public/css/"));
 });
 
-gulp.task('default', ['styles','scripts']);
+gulp.task('default', ['styles','scripts', 'app']);
 
 gulp.task('watch', ['default'], function(){
-    gulp.watch("./frontend/js/*.js", ["scripts"]);
+    gulp.watch("./frontend/js/*.js", ["app"]);
     gulp.watch("./frontend/css/*.css", ["styles"]);
 });
